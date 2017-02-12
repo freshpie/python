6ec250187484a7ec30952c95f8bed44267b1d8a6
leehan <yye572@gmail.com>
2017-02-12 22:21:17 +0900

    22

--- HelloPy/algorithm/SelectionSort.py
+++ HelloPy/algorithm/SelectionSort.py
@@ -1,10 +1,10 @@
 
 def sort(arr, index):
-	startIndex = len(arr)-index
+	startIndex = len(arr) - index
 	if startIndex < len(arr):
 		#index : 배열의 시작 인덱스
 		minNum = min(arr[startIndex:])
-		minIndex = arr.index(minNum)
+		minIndex = arr[startIndex:].index(minNum) + startIndex
 		
 		temp = arr[startIndex] 		#시작인덱스의 값
 		arr[startIndex] = minNum
@@ -14,7 +14,7 @@ def sort(arr, index):
 	
 	return arr;
 
-arr2 = [5,2,4,1,3,6]
+arr2 = [1,2,4,1,3,1]
 print(arr2)
 
 print(sort(arr2, len(arr2)))
